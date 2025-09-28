067683fc2090fe7004e2a67ed59254b9ae1f7435
another <schibkoartem@yandex.ru>
2025-09-28 19:48:12 +0400

    Добавил стили для мини картинок и их добавил

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,24 @@
     <img src="../public/icon/Yandex-Logo-PNG-HD-Images.png" class="mainImg">
   </a>
 <div class="main">
+  <div class="mainImgMany">
+    <ButtonImg link="https://yandex.ru/finance?utm_source=subclasses_product&utm_content=fin_5&utm_campaign=home_web&pers_suggest=0"
+        imageUrl="../public/icon/vecteezy_leadership-for-successful-new-idea-excellent-business-graph_9315255.png" 
+        title="Финансы"
+      />
+      <ButtonImg :img-scale="1.4" link="https://yandex.ru/realty/?utm_source=subclasses_realty&utm_content=realty&utm_campaign=home_web"
+        imageUrl="../public/icon/vecteezy_3d-interior-design_10880101.png" 
+        title="Квартиры"
+      />
+      <ButtonImg :img-scale="1.22" link="https://yandex.ru/products?utm_source=subclasses_product&utm_content=ecom&utm_campaign=home_web&pers_suggest=0"
+        imageUrl="../public/icon/vecteezy_3d-check-product_28047017.png" 
+        title="Товары"
+      />
+      <ButtonImg link="https://yandex.ru/medicine?utm_source=subclasses_product&utm_content=med_doc&utm_campaign=home_web&pers_suggest=0"
+        imageUrl="../public/iпcon/vecteezy_medical-red-first-aid-kit-with-medicine-on-transparent_54044054.png" 
+        title="Медицина"
+      />
+  </div>
   <div class="search">
     <Search/>
   </div>
@@ -11,6 +29,7 @@
 </template>
 
 <script setup lang="ts">
+import ButtonImg from './UI/ButtonImg.vue';
 import Search from './views/Search.vue';
 </script>
 
@@ -20,6 +39,7 @@ import Search from './views/Search.vue';
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 }
 .mainImg{
   position: fixed;
@@ -33,5 +53,8 @@ import Search from './views/Search.vue';
 .mainImg:hover{
   transform: scale(1.2)
 }
-
+.mainImgMany{
+  display: flex;
+  align-items: center;
+}
 </style>
--- src/UI/ButtonImg.vue
+++ src/UI/ButtonImg.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>
-        <img class="img" src="">
-        <h5></h5>
+    <div style="margin: 0 10px 30px 0;">
+        <a :href="link" style="text-decoration: none; color: white;">
+        <img :style="imgStyle" :href="link" class="img" :src="imageUrl" :alt="title">
+         <h5 class="textInImg">{{ title }}</h5>
+        </a>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
 
+interface Props {
+  imageUrl: string
+  title: string
+  link: string
+  imgScale?: number 
+}
+
+const props = defineProps<Props>()
+
+const imgStyle = computed(() => {
+  return {
+    transform: props.imgScale ? `scale(${props.imgScale})` : 'none'
+  }
+})
 </script>
 
 <style scoped>
-
+.img{
+  height: 30px;
+  margin: 5px 30px;
+  cursor: pointer;
+  transition: all 1s;
+} 
+.textInImg{
+    margin: 0px 0px 10px 20px;
+    font-weight: lighter;
+}
 </style>
